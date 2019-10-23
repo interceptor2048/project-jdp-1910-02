ab6fbedde0b84fe5f002448400a680efa3667bd1
kwisniowski <wisniowski.kacper@gmail.com>
2019-10-23 23:31:34 +0200

    UserController created

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,4 +1,26 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.user.UserDto;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping ("/v1/ecommercee")
 public class UserController {
+
+    @RequestMapping(method = RequestMethod.POST, value="createUser", consumes = APPLICATION_JSON_VALUE)
+    public void createUser (@RequestBody UserDto userDto) {
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value ="blockUser")
+    public UserDto blockUser (@RequestParam String userId) {
+        return new UserDto("testUser","testUserId");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value="getToken")
+    public String generateToken (@RequestParam String userId) {
+        return "testToken";
+    }
+
 }
