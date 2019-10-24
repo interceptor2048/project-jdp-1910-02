d0f28cc4532137afab33e9f634c6d4fa64b41820
pc <seweryn.olko@gmail.com>
2019-10-25 00:04:31 +0200

    I corrected my task JDP191002-4, especially OrderController

---
+++ src/main/java/com/kodilla/ecommercee/Order.java
@@ -0,0 +1,7 @@
+package com.kodilla.ecommercee;
+
+public class Order {
+    private Long id;
+    private String title;
+    private String content;
+}
--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -3,15 +3,16 @@ package com.kodilla.ecommercee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("v1/ecommercee")
+@RequestMapping("/v1/ecommercee")
 public class OrderController {
 
     @RequestMapping(method = RequestMethod.GET, value = "getOrders")
     public List<OrderDto> getOrders() {
-        return getOrders();
+        return new ArrayList<>();
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createOrder")
@@ -20,12 +21,12 @@ public class OrderController {
 
     @RequestMapping(method = RequestMethod.GET, value = "getOrder")
     public OrderDto getOrder(@RequestParam Long orderId) {
-        return getOrder(orderId);
+        return new OrderDto(1L, "test_title", "test_content");
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "updateOrder")
     public OrderDto updateOrder(@RequestBody OrderDto orderDto) {
-        return updateOrder(orderDto);
+        return new OrderDto(1L, "Edited test title", "test content");
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteOrder")
