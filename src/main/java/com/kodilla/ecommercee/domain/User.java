af76fc15e1399db5cb2b50923f1b96209d9cec6b
kwisniowski <wisniowski.kacper@gmail.com>
2019-10-28 23:47:10 +0100

    23:47

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,26 +1,33 @@
 package com.kodilla.ecommercee.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table (name = "user")
 public class User {
+
+    @Column (name="name")
+    @NotNull
     private String userName;
-    private String userId;
+
+    @Column (name = "id")
+    @Id
+    @GeneratedValue
+    @NotNull
+    private Long userId;
+
+    @Column (name = "isBlocked")
     private boolean isBlocked;
-
-    public User(String userName, String userId) {
-        this.userName = userName;
-        this.userId = userId;
-        this.isBlocked = false;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
 }
--- src/main/java/com/kodilla/ecommercee/domain/dto/CartDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/CartDto.java
@@ -1,5 +1,19 @@
 package com.kodilla.ecommercee.domain.dto;
 
-// to fill-up at implementation
+import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class CartDto {
+    private long id;
+    private User user;
+    private List<Product> products;
 }
