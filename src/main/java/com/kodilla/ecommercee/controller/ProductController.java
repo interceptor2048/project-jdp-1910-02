1069b6191919afd25c906f08b42493a8b2b86a11
szpila127 <inglot.seb@gmail.com>
2019-11-04 22:09:35 +0100

    Mapper, Service, Repository, ProductController

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/ecommercee/product")
+@RequestMapping("/v1/ecommercee")
 public class ProductController {
 
     @Autowired
