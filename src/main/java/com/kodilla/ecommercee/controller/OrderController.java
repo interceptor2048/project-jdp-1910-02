2863a905de5dbaf0bdd3c1518ab29b2c5436a019
pc <seweryn.olko@gmail.com>
2019-11-04 20:30:07 +0100

    a few changes in OrderMapper

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/order")
+@RequestMapping("/v1/ecommercee")
 public class OrderController {
 
     @Autowired
