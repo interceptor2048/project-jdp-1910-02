f9be95ead5d3fca01762ccd69dcfeb6c5d0def48
kwisniowski <wisniowski.kacper@gmail.com>
2019-11-05 00:17:37 +0100

    changes in userDto

--- src/main/java/com/kodilla/ecommercee/domain/dto/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/UserDto.java
@@ -1,29 +1,15 @@
 package com.kodilla.ecommercee.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
+@Getter
+@AllArgsConstructor
 public class UserDto {
     private String userName;
     private Long userId;
     private boolean isBlocked;
 
-    public UserDto(String userName, Long userId, Boolean isBlocked) {
-        this.userName = userName;
-        this.userId = userId;
-        this.isBlocked = isBlocked;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public boolean isBlocked() {
-        return isBlocked;
-    }
 }
