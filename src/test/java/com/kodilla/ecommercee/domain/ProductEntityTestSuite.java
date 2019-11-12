26ef7163f1e345df4be98a39218b0a1ba9764ae0
kwisniowski <wisniowski.kacper@gmail.com>
2019-11-12 20:35:40 +0100

    H2 db server for testing

--- src/test/java/com/kodilla/ecommercee/domain/ProductEntityTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/ProductEntityTestSuite.java
@@ -3,11 +3,15 @@ import com.kodilla.ecommercee.repository.GroupRepository;
 import com.kodilla.ecommercee.repository.ProductRepository;
 import com.kodilla.ecommercee.service.GroupService;
 import com.kodilla.ecommercee.service.ProductService;
+import org.h2.tools.Server;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.SQLException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,24 +19,29 @@ public class ProductEntityTestSuite {
 
     @Autowired
     private ProductService productService;
-    @Autowired
-    private ProductRepository productRepository;
 
     @Autowired
     private GroupService groupService;
-    @Autowired
-    private GroupRepository groupRepository;
+
+    @BeforeClass
+    public static void initTest() throws SQLException {
+        Server webServer = Server.createWebServer("-web",
+                "-webAllowOthers", "-webPort", "8082");
+        webServer.start();
+    }
 
     @Test
     public void testProductEntity() {
         //Given
         Group rtv = new Group("rtv","Produkty RTV");
-        Product monitor = new Product (2323L,"name",3.22,"desc",null,null);
+        Product monitor = new Product (2323L,"name",3.22,"description",null,null);
         rtv.getProducts().add(monitor);
         monitor.setProductGroup(rtv);
         //When
         groupService.saveGroup(rtv);
         productService.saveProduct(monitor);
         //Then
+
+        while(true);
     }
 }
