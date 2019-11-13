28307be3b86b3a4b1547b57a7b85a329ef65e1a5
kwisniowski <wisniowski.kacper@gmail.com>
2019-11-13 21:31:20 +0100

    Sonar imports removed

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -3,8 +3,6 @@ package com.kodilla.ecommercee.repository;
 import com.kodilla.ecommercee.domain.Group;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
