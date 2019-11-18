07b4e57a960f3c5df46ce8dc0e3468f776e6d8d0
Suok <suoku667@gmail.com>
2019-11-18 20:55:38 +0100

    cleaned CartController

--- src/main/java/com/kodilla/ecommercee/service/CartService.java
+++ src/main/java/com/kodilla/ecommercee/service/CartService.java
@@ -1,11 +1,16 @@
 package com.kodilla.ecommercee.service;
 
+import com.kodilla.ecommercee.controller.OrderController;
 import com.kodilla.ecommercee.domain.Cart;
+import com.kodilla.ecommercee.domain.Order;
 import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.domain.dto.OrderDto;
+import com.kodilla.ecommercee.mapper.OrderMapper;
 import com.kodilla.ecommercee.repository.CartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +19,12 @@ public class CartService {
 
     @Autowired
     CartRepository cartRepository;
+
+    @Autowired
+    OrderMapper orderMapper;
+
+    @Autowired
+    OrderController orderController;
 
     public List<Cart> getCarts() {
         return cartRepository.findAll();
@@ -27,15 +38,39 @@ public class CartService {
         return cartRepository.findById(cartId);
     }
 
-    public void deleteCart(final Long cartId) {
-        cartRepository.deleteById(cartId);
-    }
-
     public List<Product> getProducts(final Long cartId) {
-        return cartRepository.findById(cartId).get().getProducts();
+        if (cartRepository.findById(cartId).isPresent()) {
+            return cartRepository.findById(cartId).get().getProducts();
+        }
+        return new ArrayList<>();
     }
 
     public void addProduct(final Long cartId, final Product product) {
-        cartRepository.findById(cartId).get().getProducts().add(product);
+        if (cartRepository.findById(cartId).isPresent()) {
+            Cart cart = cartRepository.findById(cartId).get();
+            cart.getProducts().add(product);
+            cartRepository.save(cart);
+        }
+    }
+
+    public void createOrder(final Long cartId) {
+        StringBuilder sb = new StringBuilder();
+        Cart cart = new Cart();
+        if (cartRepository.findById(cartId).isPresent()) {
+            cart = cartRepository.findById(cartId).get();
+        }
+        double total = cart.getProducts().stream().map(Product::getPrice)
+                .mapToDouble(Double::doubleValue).sum();
+        OrderDto newOrder = orderMapper.mapToOrderDto(new Order(sb.append(cart.getUser().getUserName())
+                .append("_").append(cart.getId()).toString(), total, cart.getUser(), cart));
+        orderController.createOrder(newOrder);
+    }
+
+    public void deleteProduct(final Long cartId, final Long productId) {
+        if (cartRepository.findById(cartId).isPresent()) {
+            Cart cart = cartRepository.findById(cartId).get();
+            cart.getProducts().removeIf(t -> productId.equals(t.getId()));
+            cartRepository.save(cart);
+        }
     }
 }
